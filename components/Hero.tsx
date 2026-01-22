5dc3ca1b45680188afe5696c2fc9c86b20f11371
Manish Taram <manish@example.com>
2026-01-23 03:01:54 +0530

    Change wavy background color from teal (#7dd3d0) to light green (#D8E6AD)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -79,7 +79,7 @@ const Hero: React.FC = () => {
         {/* Top half - light teal wavy */}
         <div className="absolute top-0 left-0 right-0 h-[110px] sm:h-1/2 overflow-hidden" style={{ opacity: isMobileViewport ? 0.72 : 1, filter: isMobileViewport ? 'saturate(1.15) contrast(1.05)' : 'saturate(1.05) contrast(1.02)' }}>
           <svg className="w-full h-full block" viewBox="0 0 1440 320" preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg">
-            <path fill="#7dd3d0" d="M0,224L48,213.3C96,203,192,181,288,170.7C384,160,480,160,576,154.7C672,149,768,139,864,117.3C960,96,1056,64,1152,58.7C1248,53,1344,75,1392,85.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z" />
+            <path fill="#D8E6AD" d="M0,224L48,213.3C96,203,192,181,288,170.7C384,160,480,160,576,154.7C672,149,768,139,864,117.3C960,96,1056,64,1152,58.7C1248,53,1344,75,1392,85.3L1440,96L1440,0L1392,0C1344,0,1248,0,1152,0C1056,0,960,0,864,0C768,0,672,0,576,0C480,0,384,0,288,0C192,0,96,0,48,0L0,0Z" />
           </svg>
         </div>
 
